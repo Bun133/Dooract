545766a926fa8bbef9942f1ba16e5517db06f8b1
bun133 <bun133.work@gmail.com>
2022-03-19 21:51:08 +0900

    Add Firebase

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
-import {ref, get, set} from "firebase/database";
+import {ref, get, set, update, runTransaction} from "firebase/database";
 import {handleRequestAdmin} from "./dooractUtils";
+import {DataSnapshot, TransactionResult} from "@firebase/database";
 
 
 const firebaseConfig = require("../../../config/firebase-admin.json");
@@ -14,42 +15,100 @@ const app = admin.initializeApp({
 
 export const getRealTimeDB = functions.https.onRequest((request, response) => {
     handleRequestAdmin(request, response, () => {
-        const db = app.database();
-        const name = request.body["dbPath"];
-        const r = ref(db, name);
-        get(r).then(data => {
-            response.status(200).send({data: data});
+        getRealTimeDBFunc(request.body["dbPath"]).then(data => {
+            response.status(200).send({"status": true, data: data});
         }).catch(error => {
-            response.status(500).send({error: error});
+            response.status(500).send({"status": false, error: error});
         });
     })
 });
+
+/**
+ * Need To Auth
+ */
+function getRealTimeDBFunc(dbPath: string): Promise<DataSnapshot> {
+    const db = app.database();
+    const r = ref(db, dbPath);
+    return get(r);
+}
 
 /**
  * Force Set Data using Admin SDK
  */
 export const setRealTimeDB = functions.https.onRequest((request, response) => {
     handleRequestAdmin(request, response, () => {
-        const db = app.database();
-        let name = request.body["dbPath"];
-        let data = request.body["data"];
-        if (name && data) {
-            let r = ref(db, name)
-            set(r, data).then(data => {
-                response.status(200).send({
-                    "status": true
-                })
-            }).catch(err => {
-                response.status(500).send({
-                    "status": false,
-                    "error": err
-                })
+        setRealTimeDBFunc(request.body["dbPath"], request.body["data"]).then(data => {
+            response.status(200).send({
+                "status": true
             })
-        } else {
+        }).catch(err => {
             response.status(400).send({
                 "status": false,
-                "error": "dbName and data are required"
+                "error": err
             })
-        }
+        })
     })
 });
+
+/**
+ * Need To Auth
+ */
+function setRealTimeDBFunc(dbPath: string, data: any): Promise<void> {
+    if (dbPath && data) {
+        const db = app.database();
+        const r = ref(db, dbPath);
+        return set(r, data);
+    }
+    return Promise.reject("dbPath and data are required");
+}
+
+export const updateRealTimeDB = functions.https.onRequest((request, response) => {
+    handleRequestAdmin(request, response, () => {
+        updateRealTimeDBFunc(request.body["dbPath"], request.body["data"]).then(data => {
+            response.status(200).send({
+                "status": true
+            })
+        }).catch(err => {
+            response.status(400).send({
+                "status": false,
+                "error": err
+            })
+        })
+    })
+});
+
+function updateRealTimeDBFunc(dbPath: string, data: object): Promise<void> {
+    if (dbPath && data) {
+        const db = app.database();
+        const r = ref(db, dbPath);
+        return update(r, data);
+    }
+    return Promise.reject("dbPath and data are required");
+}
+
+/*
+export const transactionRealTimeDB = functions.https.onRequest((request, response) => {
+    handleRequestAdmin(request, response, () => {
+        transactionRealTimeDBFunc(request.body["dbPath"], eval(request.body["data"])).then(data => {
+            response.status(200).send({
+                "status": true
+            })
+        }).catch(err => {
+            response.status(400).send({
+                "status": false,
+                "error": err
+            })
+        })
+    })
+});
+*/
+
+// @ts-ignore
+function transactionRealTimeDBFunc(dbPath: string, data: (arg0: any) => unknown): Promise<TransactionResult> {
+    if (dbPath && data) {
+        const db = app.database();
+        const r = ref(db, dbPath);
+        return runTransaction(r, data);
+    }
+    return Promise.reject("dbPath and data are required");
+}
